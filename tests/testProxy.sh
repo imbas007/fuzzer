606b6007c16947b24c1251983932c12f1ff7bd7d
Dušan Panić <dpanic@gmail.com>
2023-01-07 16:29:54 +0100

    chore: change path

--- tests/testProxy.sh
+++ tests/testProxy.sh
@@ -6,6 +6,6 @@ go run main.go \
     -w wordlists/big.txt \
     -u http://www.google.com/FUZZ \
     -fc 403,404 \
-    -o tmp/standard.json \
+    -o tmp/proxy.json \
     -p http://127.0.0.1:9000 \
     -X GET
